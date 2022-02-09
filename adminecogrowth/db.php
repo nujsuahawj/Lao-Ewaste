1bff338e9a46e92cb1e7111e3735dcb5fc500c15
Nujsua Hawj <nousainther@gmail.com>
2022-02-09 13:41:37 +0700

    done v1, add seo and db user and password, by ajck sainther

--- adminecogrowth/db.php
+++ adminecogrowth/db.php
@@ -1,9 +1,9 @@
 <?php
 	// Database credentials
 	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'laos-ewaste');
+	define('DB_USERNAME', 'u830503425_jacksainther');
+	define('DB_PASSWORD', '*Ac$HMBj01');
+	define('DB_NAME', 'u830503425_ecogrowthlao');
 
 	// Attempt to connect to MySQL database
 	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
